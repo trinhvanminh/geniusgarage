2658ccf0e9d060457f3e6a1dfdd4eb7efb2f26be
minh.trinh 🐛 <tvminh2611@gmail.com>
2026-02-25 23:33:10 +0700

    fix(tests): uncomment coverage configuration in Vitest setup

--- packages/utils/vitest.config.ts
+++ packages/utils/vitest.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true, // No need to import describe, it, expect in every test
-    // coverage: {
-    //   provider: "v8",
-    //   reporter: ["text", "html"],
-    // },
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+    },
   },
 });
